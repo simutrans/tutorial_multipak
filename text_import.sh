6cd22f7174be80062d2ae53c992548ae4a4e43c4
Roboron3042 <robertoms258@gmail.com>
2021-08-21 22:06:11 +0200

    Add import script

---
+++ text_import.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PAK=pak64
+wget -O texts.zip https://makie.de/translator/data/tab/language_pack-Scenario+Tutorial+$PAK.zip
+mkdir tmp && cd tmp
+unzip ../texts.zip
+rsync -avr --exclude='en.tab' --exclude='_objectlist.txt' --exclude='_translate_users.txt' * ..
+cd ..
+rm -r tmp
+rm texts.zip
+git diff
+select yn in "Commit translations and push" "Keep changes but don't commit" "Cancel and reset"; do
+    case $yn in
+        "Commit translations and push" ) git add; git commit -m "Translation: Import texts from Simutranslator"; git push; break;;
+		"Keep changes but don't commit" ) break;;
+        "Cancel and reset" ) git reset; break;;
+    esac
+done
