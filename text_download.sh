c22b4fa43908083410ec5da4711f897b93720faf
Andarix <esithera@mail.de>
2026-01-10 09:40:44 +0100

    ADD script download texts   Windows command in console: wsl ./text_download

---
+++ text_download.sh
@@ -0,0 +1,2 @@
+wget -q --delete-after https://simutrans-germany.com/translator_page/scenarios/scenario_5/download.php
+wget -O texts.zip https://simutrans-germany.com/translator_page/scenarios/scenario_5/data/language_pack-Scenario+Tutorial+multipak.zip
